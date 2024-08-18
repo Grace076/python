cc8938a7911b5224bf3c5524b3f00cf1eb0547d9
Grace076 <76636371+Grace076@users.noreply.github.com>
2024-08-18 03:22:27 +0200

    Assignment 1  python

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input('please enter your name:')
+age = input('please enter your age:')
+location =input('enter your location:')
+print('thank your for entering your details,procced to the next one')
+print (f'Hello {name},you are today {age}years,you live in {location}')
+
